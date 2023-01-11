3405ea214209035a829b7ec1179c12fbf3fba40e
zamanien <tari0048@stud.kea.dk>
2023-01-12 00:35:41 +0100

    heroku test

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
+export default ({ mode }) => {
+  Object.assign(process.env, loadEnv(mode, process.cwd(), ''))
+  
+
+
 // https://vitejs.dev/config/
-export default defineConfig({
+return  defineConfig({
   plugins: [react()],
   server: {
     watch: {
@@ -10,6 +16,7 @@ export default defineConfig({
     },
     host: true, // Here
     // strictPort: true,
-    port: 5173
+    port: process.env.PORT as unknown as number
   }
 })
+} 
